77232994a570a83a49053f706d3d0c2ee18778b5
mjisblessed <mithasjanbade@gmail.com>
2024-12-20 02:03:33 +0530

    updated login and react base

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div style={{display: 'flex', justifyContent: 'center'}}>
+      <h1 style={{ color: 'white', boxShadow: 'unset'}}>DORMIFY</h1>
+    </div>
+  );
+}
+
+export default App
