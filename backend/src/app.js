11b1575e3c22d336b59d0e3befba8d10fb117a52
Vidhi Sethi <sethi.vidhi@gmail.com>
2024-12-20 19:36:31 +0530

    made proper backend structure

---
+++ backend/src/app.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { connectDb } from './config/db.js';
+import User from './models/users.model.js';
+import cors from 'cors';
+import authRoutes from './routes/authRoutes.js';
+
+const app = express();
+
+//middleware to parse json from requests i think
+app.use(express.json());
+
+//middleware to enable cors
+app.use(cors());
+
+//routes
+app.use("/api/auth", authRoutes);
+
+
+//start listening
+app.listen(5090, () => {
+    connectDb();
+    console.log("Server started at http://127.0.0.1:5090");
+});
--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,31 +1,27 @@
-import express from 'express';
+import {request, response} from 'express';
 import dotenv from 'dotenv';
-import { connectDb } from './config/db.js';
-import User from './models/users.model.js';
-
+import User from '../models/users.model.js';
+// import cors from 'cors';
 dotenv.config();
-const app = express();
-app.use(express.json());
-app.set("view engine", "hbs");
-app.use(express.urlencoded({extended:false}));
 
-app.get('/', (request, response) => {
-    // response.render('home');
-    response.send('home page');
-})
+// app.use(cors());
+// app.get('/', (request, response) => {
+//     // response.render('home');
+//     response.send('home page');
+// })
 
-app.get('/login', (request, response) => {
-    // response.render('login');
-    response.send(`login page! if you haven't made an account yet, sign up!`);
+// app.get('/login', (request, response) => {
+//     // response.render('login');
+//     response.send(`login page! if you haven't made an account yet, sign up!`);
     
-})
+// })
 
-app.get('/signup', (request, response) => {
-    // response.render('signup');
-    response.send(`creating a new account! this is the signup page`);
-})
+// app.get('/signup', (request, response) => {
+//     // response.render('signup');
+//     response.send(`creating a new account! this is the signup page`);
+// })
 
-app.post('/signup', async (request, response) => {
+export const signup =  async (request, response) => {
     console.log("Request Body:", request.body);
 
     try {
@@ -51,9 +47,9 @@ app.post('/signup', async (request, response) => {
         console.error("Error during signup:", error);
         response.status(500).send("An error occurred during signup.");
     }
-});
+};
 
-app.post('/login', async (request, response) => {
+export const login = async (request, response) => {
     try {
         const existingName = await User.findOne({name : request.body.name });
         if (!existingName) {
@@ -79,10 +75,5 @@ app.post('/login', async (request, response) => {
         console.error("Error during login:", error);
         response.status(500).send("An error occurred during login.");
     }
-})
+};
 
-
-app.listen(5090, () => {
-    connectDb();
-    console.log("Server started at http://127.0.0.1:5090");
-});
