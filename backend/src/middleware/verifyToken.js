ba02184c6ea6e25c782d1c54e52a8dc149534c05
Vidhi Sethi <sethi.vidhi@gmail.com>
2024-12-24 17:31:07 +0530

    data hiding :)

--- backend/src/middleware/verifyToken.js
+++ backend/src/middleware/verifyToken.js
@@ -1,6 +1,5 @@
 import jwt from 'jsonwebtoken'
-import bcrypt from 'bcryptjs';
-export const JWT_SECRET = "secretkey"
+export const JWT_SECRET = process.env.JWT_SECRET_KEY
 
 export const authenticateToken = (request, response, next) => {
     const authHeader = request.headers['authorization'];
@@ -9,7 +8,8 @@ export const authenticateToken = (request, response, next) => {
     if (!token) return response.status(401).send("Access Denied: No Token Provided!");
 
     jwt.verify(token, JWT_SECRET, (err, user) => {
-        if (err) return response.status(403).send("Invalid Token!");
+        if (err)
+            return response.status(403).send("Invalid Token!");
         request.user = user; 
         next();
     });
