e180a6c5bfddd85e0e0556b015f9290fb6467e02
NoorArora07 <noorarora.bt23cse@pec.edu.in>
2024-12-27 18:42:30 +0530

    mess routes for calender events for warden side

---
+++ backend/src/routes/wardenMessRoutes.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import { verifyToken } from '../middleware/verifyToken.js';
+import {
+    getEvents,
+    createEvent,
+    updateEvent,
+    deleteEvent,
+} from '../controllers/warden_mess.js';
+
+const router = express.Router();
+
+router.get('/getEvent', verifyToken, getEvents);
+router.post('/createEvent', verifyToken, createEvent);
+//router.put('updateEvent/:id', verifyToken, updateEvent);
+//router.delete('deleteEvent/:id', verifyToken, deleteEvent);
+
+export default router;
