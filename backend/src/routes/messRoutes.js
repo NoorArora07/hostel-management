9df1e7c843fc1e9714005ec67580f48a6ec0731a
NoorArora07 <noorarora.bt23cse@pec.edu.in>
2024-12-27 19:18:45 +0530

    added routes to make view events option available for users too

--- backend/src/routes/messRoutes.js
+++ backend/src/routes/messRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { verifyToken } from '../middleware/verifyToken.js';
-import { MessLeaveForm,getMessStatus,getMessLeaveDetails } from '../controllers/messController.js';
+import { getEvents,MessLeaveForm,getMessStatus,getMessLeaveDetails } from '../controllers/messController.js';
 
 const router = express.Router();
 
 router.get("/status", verifyToken, getMessStatus);
 router.post("/off", verifyToken, MessLeaveForm);
 router.get("/leave-details", verifyToken, getMessLeaveDetails);
+router.get("/getEvent", verifyToken, getEvents);
 
 export default router; 
