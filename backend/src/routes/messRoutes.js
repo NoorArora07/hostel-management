21be98c01503ef274e0bfe0f5c65dcc32c191a36
NoorArora07 <noorarora.bt23cse@pec.edu.in>
2024-12-27 16:16:38 +0530

    mess leave view routes addded!

--- backend/src/routes/messRoutes.js
+++ backend/src/routes/messRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { verifyToken } from '../middleware/verifyToken.js';
-import { MessLeaveForm,getMessStatus } from '../controllers/messController.js';
+import { MessLeaveForm,getMessStatus,getMessLeaveDetails } from '../controllers/messController.js';
 
 const router = express.Router();
 
 router.get("/status", verifyToken, getMessStatus);
 router.post("/off", verifyToken, MessLeaveForm);
+router.get("/leave-details", verifyToken, getMessLeaveDetails);
 
 export default router; 
