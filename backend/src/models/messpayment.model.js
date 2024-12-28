5019f836fc6a44b6ed52a832760098abdf095aba
NoorArora07 <noorarora.bt23cse@pec.edu.in>
2024-12-28 18:03:47 +0530

    updated model for mess payment

--- backend/src/models/messpayment.model.js
+++ backend/src/models/messpayment.model.js
@@ -1,27 +1,41 @@
 import mongoose from 'mongoose';
 
-const paymentSchema = new mongoose.Schema({
-  paymentIntentId: {
-    type: String,
-    required: true,
-  },
-  amount: {
-    type: Number,
-    required: true,
-  },
-  currency: {
-    type: String,
-    required: true,
-  },
-  status: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+const feeSchema = new mongoose.Schema({
+  studentId: { type: String, required: true },
+  status: { type: String, default: 'pending' },
+  amount: { type: Number, required: true },
 });
 
-export default mongoose.model('Payment', paymentSchema);
+export default mongoose.model('Fee', feeSchema);
+
+
+// const paymentSchema = new mongoose.Schema({
+//   paymentIntentId: {
+//     type: String,
+//     required: true,
+//   },
+//   amount: {
+//     type: Number,
+//     required: true,
+//   },
+//   currency: {
+//     type: String,
+//     required: true,
+//   },
+//   status: {
+//     type: String,
+//     required: true,
+//     enum: ['paid', 'pending', 'transaction_failed'],
+//   },
+//   failureReason: {
+//     type: String,
+//     default: null, 
+//   },
+//   createdAt: {
+//     type: Date,
+//     default: Date.now,
+//   },
+// });
+
+//export default mongoose.model('Payment', paymentSchema);
 
