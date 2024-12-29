a4df52aa38e9689ceae1cc4e139a77c719102add
Vidhi Sethi <sethi.vidhi@gmail.com>
2024-12-30 02:39:36 +0530

    fixed some git conflicts and worked a bit more on basic apis (not done!!)

--- backend/src/controllers/roomAllocationController.js
+++ backend/src/controllers/roomAllocationController.js
@@ -25,8 +25,8 @@ const selectEmpty = async (request, response) => {
     //ek empty room ko select karna hai
 
     //request -> {roomNumber, allowWaitingList}
-    const roomNo = request.roomNumber;
-    const allowWaitingList = request.allowWaitingList;
+    const roomNo = request.body.roomNumber;
+    const allowWaitingList = request.body.allowWaitingList;
     const sid = request.user.sid;
     const name = request.user.name;
     
@@ -56,6 +56,13 @@ const selectEmpty = async (request, response) => {
         console.log(result);
         if (result.acknowledged)
             console.log("hurray");
+
+        let roomChosen = await room.findOne({
+            roomNumber: roomNo
+        })
+
+        roomChosen.numberOfOccupants++;
+        
         
         response.send(200).json({
            "selected": true
@@ -70,5 +77,53 @@ const selectEmpty = async (request, response) => {
 }
 
 const selectAnother = async (request, response) => {
-    //logic to select another room
+    const isWaitingList = request.body.isWaitingList
+    const roomNo = request.body.roomNumber;
+    const sid = request.user.sid;
+    const name = request.user.name;
+    
+    const info = {
+        sid: sid,
+        name: name,
+        branch: branch
+    }
+
+    if (!isWaitingList) {
+        try {
+            const userDetails = await userDetails.findOne( {sid: sid} );
+            if (!userDetails) {
+                console.log("no such user exists tf lmaoo");
+                response.status(500).send("trying to find a room for a user that doesn't exist o_O O_o");
+            }
+
+            const branch = userDetails.branch;
+
+            let data = new person({
+                name: name,
+                sid: sid,
+                branch: branch,
+                roomSelected: "true",
+                allowWaitingList: false,
+                roomNumber: roomNo
+            })
+            const result = await data.save();
+            console.log(result);
+            if (result.acknowledged)
+                console.log("hurray");
+
+            let roomChosen = await room.findOne({
+                roomNumber: roomNo
+            })
+
+            roomChosen.numberOfOccupants++;
+            roomChosen.occupantsDetails.push(info);
+            
+            response.send(200).json({
+            "selected": true
+            }) //temporary response, this could change aage jaake
+        } catch (error) {
+            console.log("Error while trying to select an empty room!");
+            response.status(500).send("error while selecting an empty room", error);    
+        }
+    }
 }
