7fd860cc34d7d968899b62444419b7c7898c8769
NoorArora07 <noorarora.bt23cse@pec.edu.in>
2024-12-29 14:30:32 +0530

    added models for complaint

---
+++ backend/src/models/complaints.model.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const complaintSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true, // sent date
+  },
+  status: {
+    type: String,
+    enum: ["pending", "resolved", "rejected"],
+    default: "pending",
+  },
+});
+
+const studentSchema = mongoose.Schema({
+  sid: {
+    type: String,
+    required: true,
+    unique: true, 
+  },
+  branch: {
+    type: String,
+    required: true,
+  },
+  roomNumber: {
+    type: String,
+    required: true,
+  },
+  complaints: {
+    type: [complaintSchema], // Array of complaint objects
+    default: [],
+  },
+});
+
+export default mongoose.model("Complaint", studentSchema);
