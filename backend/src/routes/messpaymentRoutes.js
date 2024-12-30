11001c93538c70227ebb8d523c7f1699bcef1fd3
NoorArora07 <noorarora.bt23cse@pec.edu.in>
2024-12-30 20:20:11 +0530

    mess payment finally working successfully at backend!!!

--- backend/src/routes/messpaymentRoutes.js
+++ backend/src/routes/messpaymentRoutes.js
@@ -1,13 +1,12 @@
 import express from 'express';
-import { initiatePayment,updateFeeStatus,getFeeStatus, generateStripeSignature } from '../controllers/messpaymentControllers.js';
+import { initiatePayment,updateFeeStatus,getFeeStatus } from '../controllers/messpaymentControllers.js';
 import { verifyToken } from '../middleware/verifyToken.js';
 
 const router = express.Router();
 
 router.post('/paynow',verifyToken, initiatePayment);
-router.post('/generate-signature', generateStripeSignature);
-//router.post('/updateStatus',verifyToken, updateFeeStatus);
-//router.get('/status/:studentId',verifyToken, getFeeStatus);
+router.post('/updateStatus',verifyToken, updateFeeStatus);
+router.get('/status',verifyToken, getFeeStatus);
 
 
 
