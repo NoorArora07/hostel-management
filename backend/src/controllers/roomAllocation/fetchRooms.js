76ffbb2a0c1193ca7d1412f8ff962f5bd081d1d8
Vidhi Sethi <sethi.vidhi@gmail.com>
2025-01-01 13:36:54 +0530

    added one more api

--- backend/src/controllers/roomAllocation/fetchRooms.js
+++ backend/src/controllers/roomAllocation/fetchRooms.js
@@ -21,3 +21,28 @@ export const fetchRooms = async (request, response) => {
         return response.status(500).send("error while fetching the rooms");
     }
 }
+
+export const viewPartOf = async (request, response) => {
+    try {
+        const usersid = request.user.sid;
+        const roomNumber = request.params.roomNumber;
+
+        const personDetails = await person.findOne({sid: usersid});
+        const roomDetails = await room.findOne({roomNumber: roomNumber});
+
+        if (personDetails.roomNumber === roomNumber) {
+            if (personDetails.roomSelected === "pending")
+                return response.status(200).json({"occupant": false, "allowWaitingList": true, "inWaitingList": true});
+            else if (roomDetails.allowWaitingList === true)
+                return response.status(200).json({"occupant": true, "allowWaitingList": true, "inWaitingList": false});
+            else
+                return response.status(200).json({"occupant": true, "allowWaitingList": false, "inWaitingList": false });
+        }
+        else
+            return response.status(200).json({"occupant": false, "inWaitingList": false })
+
+    } catch (error) {
+        console.log("error while trying to view what part you are of the room ", error);
+        return response.status(500).send("error while viewing part of the rooms!");
+    }
+}
--- backend/src/routes/roomAllocRoutes.js
+++ backend/src/routes/roomAllocRoutes.js
@@ -9,6 +9,7 @@ import { declineInWaitingList } from '../controllers/roomAllocation/waitingList.
 import { acceptInWaitingList } from '../controllers/roomAllocation/waitingList.js';
 import { viewWaitingList } from '../controllers/roomAllocation/waitingList.js';
 import { leaveWaitingList } from '../controllers/roomAllocation/waitingList.js';
+import { viewPartOf } from '../controllers/roomAllocation/fetchRooms.js';
 
 const router = express.Router();
 
@@ -20,5 +21,6 @@ router.get("/waiting-list/", verifyToken, viewWaitingList);
 router.patch("/waiting-list/accept", verifyToken, acceptInWaitingList);
 router.patch("/waiting-list/decline", verifyToken, declineInWaitingList);
 router.patch("/waiting-list/leave", verifyToken, leaveWaitingList);
+router.get("/get-info/:roomNumber", verifyToken, viewPartOf);
 
 export default router
