0073075adcb013ee2fafb827d18c5e2f2883f64b
NoorArora07 <noorarora.bt23cse@pec.edu.in>
2025-01-05 01:51:34 +0530

    incoprorated stripe in hostel fees too!

---
+++ backend/src/routes/hostelfeesRoutes.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import { initiatePayment,updateFeeStatus,getFeeStatus } from '../controllers/roomAllocation/hostelFee.js';
+import { verifyToken } from '../middleware/verifyToken.js';
+
+const router = express.Router();
+
+router.post('/paynow',verifyToken, initiatePayment);
+router.post('/updateStatus',verifyToken, updateFeeStatus);
+router.get('/status',verifyToken, getFeeStatus);
+
+export default router;
+
+
+
