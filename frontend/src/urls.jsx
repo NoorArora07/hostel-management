43ab699ead3cc4f0d63e8ec8d28548f5bad853ad
NoorArora07 <noorarora.bt23cse@pec.edu.in>
2025-01-14 23:50:26 +0530

    fixed issues

--- frontend/src/urls.jsx
+++ frontend/src/urls.jsx
@@ -1,3 +1,5 @@
 // export const baseUrl = `https://hostel-management-nqlh.onrender.com`;
 // export const baseUrl = `https://hostel-management-pvjm.onrender.com`;
-export const baseUrl = `http://127.0.0.1:5090`;
+//export const baseUrl = `http://127.0.0.1:5090`;
+
+export const baseUrl = "https://hostel-management-backend-b2ef.onrender.com"
